9cc8e41563c87292a0fa3fdba09e447070783169
Cristian <cristianm87@hotmail.com>
2021-06-03 00:45:02 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, {Component, Fragment} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// hay dos tipos de componentes dentro de react los Class y los Function
+
+class App extends Component {
+  render() {
+    
+    return (
+      <>
+        <header>
+          <nav>
+            <div className="container">
+              <div className="logo">
+                <a href="index.html"><img src="./images/logo.png" alt="logo"/></a>
+              </div>
+              <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="index.html">About</a></li>
+                <li><a href="index.html">Help</a></li>
+                <li><a href="index.html">Contact</a></li>            
+              </ul>
+            </div>
+          </nav>
+        </header>
+        <main>
+          <h1>Bienvenide</h1>
+          <img src="./images/logo.png" alt="" />
+        </main>
+        
+      </>
+    )
+  }
 }
 
 export default App;
